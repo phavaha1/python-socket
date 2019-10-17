05167e698f85f127b5811fbfb07b5aa18e4fdb33
phavaha1 <haupv@vietnamlab.vn>
2019-10-17 18:00:40 +0700

    first commit

---
+++ src/server.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('socket created successfully')
+except socket.error:
+    print('socket creation failed')
+    sys.exit()
+
+port = 12345
+
+s.bind('', port)
+
+s.listen(5)
+print('socket is listening')
+
+while True:
+    c, addr = s.accept()
+    print('got connection from {addr}')
+
+    c.send('Thank for connection')
+    c.close()
